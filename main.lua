9dc64e8ce62c9cccff1c17548d98ee8da8934966
Felix44 <felice.dangelo2013@gmail.com>
2021-08-04 18:04:04 +0200

    fixed transparency issue in the darkness shader

--- main.lua
+++ main.lua
@@ -165,9 +165,7 @@ extern number darkness;
 vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
   vec4 pixel = Texel(texture, texture_coords );
   number average = (pixel.r+pixel.b+pixel.g)/darkness;
-  pixel.r = min(average, pixel.r);
-  pixel.g = min(average, pixel.g);
-  pixel.b = min(average, pixel.b);
+  pixel.rgb = min(pixel.rgb, average);
   return pixel * color;
 }
 ]])
