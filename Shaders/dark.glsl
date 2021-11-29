193a39792a08ddb1bfaaca6b048000b28ae0b7be
Felix44 <felice.dangelo2013@gmail.com>
2021-11-29 21:44:10 +0100

    small fix to dark shader

--- Shaders/dark.glsl
+++ Shaders/dark.glsl
@@ -4,5 +4,7 @@ uniform float scale;
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
     vec4 pixel = Texel(texture, texture_coords);
     float dist = min(distance(screen_coords.xy, pos.xy), 160 * scale);
-    return mix(pixel, vec4(0, 0, 0, pixel.a), dist / (200 * scale)) * color;
+    vec4 result = mix(pixel, vec4(0, 0, 0, pixel.a), dist / (200 * scale));
+    result.a = result.a * color.a;
+    return result;
 }
