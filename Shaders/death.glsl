d1cbe58802f235c09c36473ea73fdc9a534c6f3e
Felix44 <felice.dangelo2013@gmail.com>
2021-11-29 15:35:04 +0100

    "improved" (?) syntax in death shader

--- Shaders/death.glsl
+++ Shaders/death.glsl
@@ -1,8 +1,8 @@
-extern number darkness;
+uniform number darkness;
 
-vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
-  vec4 pixel = Texel(texture, texture_coords );
-  number average = (pixel.r+pixel.b+pixel.g)/darkness;
-  pixel.rgb = min(pixel.rgb, average);
-  return pixel * color;
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
+	vec4 pixel = Texel(texture, texture_coords);
+	number average = (pixel.r + pixel.b + pixel.g) / darkness;
+	pixel.rgb = min(pixel.rgb, average);
+	return pixel * color;
 }
