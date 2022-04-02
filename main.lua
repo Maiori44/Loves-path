dc1bcc01ec1f7c424b189013726c519abc4196df
Felix44 <felice.dangelo2013@gmail.com>
2022-04-02 16:38:59 +0200

    fix previous menu being drawn when not needed

--- main.lua
+++ main.lua
@@ -509,7 +509,7 @@ local girl = love.graphics.newImage("Sprites/girl.png")
 
 local function DrawMenu(gs)
 	local gamestate = gs or gamestate
-	if gamestate ~= laststate and statetimer < 1 then
+	if gamestate ~= laststate and statetimer < 0.99 then
 		love.graphics.translate(0, screenwidth - ((1 -statetimer) * screenwidth))
 		DrawMenu(laststate)
 		love.graphics.origin()
