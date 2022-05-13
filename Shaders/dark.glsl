4a0f7bb98fdf7e6e0b0f0adcacf4bf42034f9f3e
Felix44 <felice.dangelo2013@gmail.com>
2022-05-13 19:13:28 +0200

    random syntax improvment

--- Shaders/dark.glsl
+++ Shaders/dark.glsl
@@ -6,6 +6,6 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
     vec4 pixel = Texel(texture, texture_coords);
     float dist = min(distance(screen_coords.xy, pos.xy), 160 * scale);
     vec4 result = mix(pixel, vec4(0, 0, 0, pixel.a), dist / (light * scale));
-    result.a = result.a * color.a;
+    result.a *= color.a;
     return result;
 }
