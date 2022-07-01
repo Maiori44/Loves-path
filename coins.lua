e5de703798187c0b6ba260655ee14b7a61c3034a
Felix44 <felice.dangelo2013@gmail.com>
2022-07-01 11:17:16 +0200

    add coin

--- coins.lua
+++ coins.lua
@@ -21,6 +21,7 @@ local coins = {
 	[28] = {x = 2, y = 2, got = false},
 	[30] = {x = 18, y = 18, got = false},
 	[33] = {x = 25, y = 12, got = false},
+	[35] = {x = 1, y  = 13, got = false},
 	[39] = {x = 24, y = 20, got = false},
 }
 
