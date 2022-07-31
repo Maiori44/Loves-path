4066955c791dcc197c0d4fba0b413b8dde96f908
Felice D'Angelo <felice.dangelo2013@gmail.com>
2022-07-31 17:08:01 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+7z a "Love's Path.zip" Maps Music Shaders Sounds Sprites cache.lua cdef.c coins.lua conf.lua customhandler.lua cutscenes.lua discordRPC.lua editundo.ttf main.lua maps.lua menu.lua music.lua nativefs.lua objects.lua particles.lua player.lua
+filename="Love's Path $1"
+mkdir "$filename"
+cat Compiler/love.exe "Love's Path.zip" > "$filename/$filename.exe"
+rm "Love's Path.zip"
+cp -r Compiler/dlls/* "$filename"
+7z a "$filename.zip" "$filename"
+rm -r "$filename"
