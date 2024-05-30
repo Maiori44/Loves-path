2a3f88af795fbfdcee7bcb1a53e15b6e3b2d43f2
Maiori44 <felice.dangelo2013@gmail.com>
2024-05-31 00:53:39 +0200

    add discord rpc binary to builds

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-7z a "Love's Path.zip" Maps Music Shaders Sounds Sprites cache.lua cdef.c coins.lua conf.lua customhandler.lua cutscenes.lua discordRPC.lua editundo.ttf main.lua maps.lua menu.lua music.lua nativefs.lua objects.lua particles.lua player.lua
-filename="Love's Path $1"
-mkdir "$filename"
-cat Compiler/love.exe "Love's Path.zip" > "$filename/Love's Path.exe"
-rm "Love's Path.zip"
-cp -r Compiler/dlls/* "$filename"
-7z a "$filename.zip" "$filename"
-rm -r "$filename"
